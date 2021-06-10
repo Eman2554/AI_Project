ddd9566b72a38df7ad5688312e3acea6cad6abbc
Fatma-tech <55929067+Fatma-tech@users.noreply.github.com>
2021-06-10 20:59:47 +0200

    Add files via upload

---
+++ game_is_end_function.py
@@ -0,0 +1,17 @@
+def game_ends(Ai_side,Player_side):  ### is end
+
+    if ((sum(Ai_side[1:7]))==0)or((sum(Player_side[0:6]))==0):
+
+        if (sum(Ai_side[1:7])) == 0:
+
+            Player_side[6] = Player_side[6]+sum(Player_side[0:6])
+            Player_side[0:6] = [0, 0, 0, 0, 0, 0]
+
+        else:
+
+            Ai_side[0] = Ai_side[0] + sum(Ai_side[1:7])
+            Ai_side[1:7] =[0,0,0,0,0,0]
+
+        return True
+    else:
+        return False
