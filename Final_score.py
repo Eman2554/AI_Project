f96dda2c45e122d1eaea0af3987d98ddb158a42b
FatemaAlzhraaMouhammad <77073525+FatemaAlzhraaMouhammad@users.noreply.github.com>
2021-06-10 17:23:23 +0200

    Update Final_score.py
    Final_score  returns different score determines win or loss or equal scores regarding AI and player scores when move is fully tracing lead to ending game .

--- Final_score.py
+++ Final_score.py
@@ -1,10 +1,10 @@
 def Final_score(Ai_side,Player_side):
 
-        if Ai_side[0]>Player_side[6]:
+        if Ai_side[0]>Player_side[6]: # AI wins the player
             return 70
 
-        if Ai_side[0] == Player_side[6]:
+        if Ai_side[0] == Player_side[6]: #draw
             return 0
 
-        else:
+        else:  # player wins AI
             return -70
